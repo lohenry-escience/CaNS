0f408c825eb1f1ed7885399e633e53d8268b5e51
Louis Henry <lhenry1@int4.local.snellius.surf.nl>
2024-12-16 18:01:59 +0100

    Log

--- intel2.sh
+++ intel2.sh
@@ -2,9 +2,9 @@ INSTALL_DIR=${HOME}/software/intel/oneapi
 TOOLKIT=Base
 VERSION=2024.0.0.49564
 INSTALL_SCRIPT=l_${TOOLKIT}Kit_p_${VERSION}.sh
-wget https://registrationcenter-download.intel.com/akdlm/IRC_NAS/20f4e6a1-6b0b-4752-b8c1-e5eacba10e01/${INSTALL_SCRIPT}
+wget --progress=dot:mega https://registrationcenter-download.intel.com/akdlm/IRC_NAS/20f4e6a1-6b0b-4752-b8c1-e5eacba10e01/${INSTALL_SCRIPT}
 
-sh ./${INSTALL_SCRIPT} -a --silent --eula accept --install-dir ${INSTALL_DIR} ; rm ${INSTALL_SCRIPT}
+sh ./${INSTALL_SCRIPT} -a --silent --eula accept --install-dir ${INSTALL_DIR}; rm ${INSTALL_SCRIPT}
 echo "LoH: After download 1"
 du -hs $INSTALL_DIR
 rm -rf ${INSTALL_DIR}/documentation ${INSTALL_DIR}/samples
@@ -15,7 +15,7 @@ du -hs $INSTALL_DIR
 TOOLKIT=HPC
 VERSION=2024.0.0.49589
 INSTALL_SCRIPT=l_${TOOLKIT}Kit_p_${VERSION}_offline.sh
-wget https://registrationcenter-download.intel.com/akdlm/IRC_NAS/1b2baedd-a757-4a79-8abb-a5bf15adae9a/${INSTALL_SCRIPT}
+wget --progress=dot:mega https://registrationcenter-download.intel.com/akdlm/IRC_NAS/1b2baedd-a757-4a79-8abb-a5bf15adae9a/${INSTALL_SCRIPT}
 sh ./${INSTALL_SCRIPT} -a --silent --eula accept --install-dir ${INSTALL_DIR} --components intel.oneapi.lin.ifort-compiler; rm ${INSTALL_SCRIPT}
 
 echo "LoH: After NVIDIA"
