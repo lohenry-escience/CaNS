a82b235500c4f4d2994dbf06abaec9543fac280e
Louis Henry <lhenry1@int4.local.snellius.surf.nl>
2024-12-16 17:47:57 +0100

    Pia

--- intel2.sh
+++ intel2.sh
@@ -5,7 +5,7 @@ INSTALL_SCRIPT=l_${TOOLKIT}Kit_p_${VERSION}.sh
 wget https://registrationcenter-download.intel.com/akdlm/IRC_NAS/20f4e6a1-6b0b-4752-b8c1-e5eacba10e01/${INSTALL_SCRIPT}
 
 sh ./${INSTALL_SCRIPT} --list-components
-sh ./${INSTALL_SCRIPT} -a --silent --eula accept --install-dir ${INSTALL_DIR} --components intel.oneapi.lin.ifx; rm ${INSTALL_SCRIPT}
+sh ./${INSTALL_SCRIPT} -a --silent --eula accept --install-dir ${INSTALL_DIR} ; rm ${INSTALL_SCRIPT}
 echo "LoH: After download 1"
 du -hs $INSTALL_DIR
 rm -rf ${INSTALL_DIR}/documentation ${INSTALL_DIR}/samples
@@ -17,7 +17,7 @@ TOOLKIT=HPC
 VERSION=2024.0.0.49589
 INSTALL_SCRIPT=l_${TOOLKIT}Kit_p_${VERSION}_offline.sh
 wget https://registrationcenter-download.intel.com/akdlm/IRC_NAS/1b2baedd-a757-4a79-8abb-a5bf15adae9a/${INSTALL_SCRIPT}
-sh ./${INSTALL_SCRIPT} -a --silent --eula accept --install-dir ${INSTALL_DIR}; rm ${INSTALL_SCRIPT}
+sh ./${INSTALL_SCRIPT} -a --silent --eula accept --install-dir ${INSTALL_DIR} --components intel.oneapi.lin.ifort-compiler; rm ${INSTALL_SCRIPT}
 
 echo "LoH: After NVIDIA"
 du -hs $INSTALL_DIR
