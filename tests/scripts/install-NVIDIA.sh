3b18627a2eea649438921f808d99943434c210ee
Louis Henry <lhenry1@int4.local.snellius.surf.nl>
2024-12-17 17:59:31 +0100

    Mia

--- tests/scripts/install-NVIDIA.sh
+++ tests/scripts/install-NVIDIA.sh
@@ -75,4 +75,5 @@ echo
 
 echo "INFO: Installing now"
 df --output=avail -BG "/opt"
-./nvhpc_${NVYEAR}_${NVVERSION_B}_${NVARCH}_cuda_${CUDA_VERSION}/install
+./nvhpc_${NVYEAR}_${NVVERSION_B}_${NVARCH}_cuda_${CUDA_VERSION}/install --help
+
