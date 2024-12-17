528ba8204c2fb1f127ca6aa410d4f697660c9768
Louis Henry <lhenry1@int4.local.snellius.surf.nl>
2024-12-17 18:09:40 +0100

    Setenv

--- tests/scripts/setenv-NVIDIA.sh
+++ tests/scripts/setenv-NVIDIA.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+YEAR=2024
+NVHPC_INSTALL_DIR=$HOME/software/nvidia/hpc_sdk
+NVARCH=`uname -s`_`uname -m`; export NVARCH
+NVCOMPILERS=$NVHPC_INSTALL_DIR; export NVCOMPILERS
+MANPATH=$MANPATH:$NVCOMPILERS/$NVARCH/$YEAR/compilers/man; export MANPATH
+PATH=$NVCOMPILERS/$NVARCH/$YEAR/compilers/bin:$PATH; export PATH
+
+export PATH=$NVCOMPILERS/$NVARCH/$YEAR/comm_libs/mpi/bin:$PATH
+export MANPATH=$MANPATH:$NVCOMPILERS/$NVARCH/$YEAR/mpi:$MANPATH
+export PATH=$NVCOMPILERS/$NVARCH/$YEAR/profilers/Nsight_Systems/bin:$PATH
 #
-docker run --rm -it -v ${PWD}:/workspace nvcr.io/nvidia/nvhpc:latest /bin/bash
-cd /workspace
+# communication libraries
+#
+export LD_LIBRARY_PATH=$NVHPC_INSTALL_DIR/$NVARCH/$YEAR/compilers/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$NVHPC_INSTALL_DIR/$NVARCH/$YEAR/comm_libs/mpi/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$NVHPC_INSTALL_DIR/$NVARCH/$YEAR/math_libs/lib64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$NVHPC_INSTALL_DIR/$NVARCH/$YEAR/comm_libs/nccl/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$NVHPC_INSTALL_DIR/$NVARCH/$YEAR/comm_libs/nvshmem/lib:$LD_LIBRARY_PATH
