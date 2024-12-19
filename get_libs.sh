24aa94bc8d612d69b96ff1847b13a2bc4cb32310
Louis Henry <lhenry1@int4.local.snellius.surf.nl>
2024-12-19 22:09:38 +0100

    Correct mistake

--- get_libs.sh
+++ get_libs.sh
@@ -1,6 +1,6 @@
 mkdir -p libs
 rm -rf libs/*
-lddtree ./run/cans | awk '{print $3}' | grep -v '^$' | while read lib; do
+lddtree ./run/cans | awk '{print $3}' | tail -n +2 | grep -v '^$' | while read lib; do
     cp -P "$lib" libs/
     # Resolve the real path if it's a symbolic link
     real_path=$(readlink -f "$lib")
