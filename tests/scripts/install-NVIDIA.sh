7b74ab5dff34f7e67fc5ddd9392ba1ee2ae9adb6
Louis Henry <lhenry1@int6.local.snellius.surf.nl>
2025-02-05 11:09:38 +0100

    Pia

--- tests/scripts/install-NVIDIA.sh
+++ tests/scripts/install-NVIDIA.sh
@@ -51,15 +51,16 @@ fi
 
 NVVERSION_B=$(echo $NVVERSION_A | sed 's/\.//g')
 NVARCH=`uname -s`_`uname -m`
-wget --progress=dot:giga https://developer.download.nvidia.com/hpc-sdk/${NVVERSION_A}/nvhpc_${NVYEAR}_${NVVERSION_B}_${NVARCH}_cuda_${CUDA_VERSION}.tar.gz
+wget --progress=dot:giga https://developer.download.nvidia.com/hpc-sdk/${NVVERSION_A}/nvhpc_${NVYEAR}_${NVVERSION_B}_${NVARCH}_cuda_${CUDA_VERSION}.tar.gz -O ./NVIDIA.tar.gz
+mkdir -p /opt/nvidia
 echo "INFO: Downloaded the file"
 pwd
 df --output=avail -BG "."
-tar -xpzf nvhpc_${NVYEAR}_${NVVERSION_B}_${NVARCH}_cuda_${CUDA_VERSION}.tar.gz
+tar -xpzf ./NVIDIA.tar.gz -C /opt/nvidia
 echo "INFO: Unpacked the file"
 pwd
 df --output=avail -BG "."
-rm nvhpc_${NVYEAR}_${NVVERSION_B}_${NVARCH}_cuda_${CUDA_VERSION}.tar.gz
+rm NVIDIA.tar.gz
 pwd
 df --output=avail -BG "."
 
@@ -75,5 +76,5 @@ echo
 
 echo "INFO: Installing now"
 df --output=avail -BG "/opt"
-./nvhpc_${NVYEAR}_${NVVERSION_B}_${NVARCH}_cuda_${CUDA_VERSION}/install --help
+#./nvhpc_${NVYEAR}_${NVVERSION_B}_${NVARCH}_cuda_${CUDA_VERSION}/install --help
 
